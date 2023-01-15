8989fb27ac8f227b7da29a269b63249bba55c7e9
liang-faan <liang.faan@gmail.com>
2023-01-15 09:14:22 +0800

    initial commit

---
+++ src/main/java/com/lfa/selenium/auto/test/Application.java
@@ -0,0 +1,13 @@
+package com.lfa.selenium.auto.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
